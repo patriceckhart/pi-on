494e34f032926441b344485b43ed53bc893cefc0
patriceckhart <mail@patriceckhart.com>
2026-02-28 20:12:14 +0100

    Initial Commit

---
+++ Pi On/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pi On
+//
+//  Created by Patric Eckhart on 28.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pi On/Pi_OnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pi_OnApp.swift
+//  Pi On
+//
+//  Created by Patric Eckhart on 28.02.26.
+//
+
+import SwiftUI
+
+@main
+struct Pi_OnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
